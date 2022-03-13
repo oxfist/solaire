86e3fa34d532c9da75af8b9d828dcc977f787777
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-03-13 13:27:31 -0300

    feat(home): add meta tags with react helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Helmet } from 'react-helmet'
+
+import metaImage from '/src/images/solaire-phantom.jpg'
 import './App.css'
 
 function App() {
-
   return (
     <div className="">
+      <Helmet>
+        <title>
+          Solaire | The (jolliest) Cooperator App for "Dark Souls: The Board Game"
+        </title>
+        <meta property="og:title" content="Solaire - The Cooperator App" />
+        <meta property="og:description" content="A helper app for playing Dark Souls: The Board Game. Use on your phone or desktop browser, it works great in both!" />
+        <meta property="og:image" content="https://guides.gamepressure.com/darksouls/gfx/word/464924671.jpg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+      </Helmet>
       <header className="">
         <h1>Dark Souls</h1>
         <h2>The Board Game</h2>
