320f68251e0ddcd33701b64ace5e5a4c851d6996
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-03-13 22:19:43 -0300

    feat(linting): setup ESLint and Prettier with package.json command

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Helmet } from 'react-helmet'
 
-import metaImage from '/src/images/solaire-phantom.jpg'
 import './App.css'
 
 function App() {
@@ -8,11 +7,18 @@ function App() {
     <div className="">
       <Helmet>
         <title>
-          Solaire | The (jolliest) Cooperator App for "Dark Souls: The Board Game"
+          Solaire | The (jolliest) Cooperator App for{' '}
+          {`"Dark Souls: The Board Game"`}
         </title>
         <meta property="og:title" content="Solaire - The Cooperator App" />
-        <meta property="og:description" content="A helper app for playing Dark Souls: The Board Game. Use it on your phone or desktop browser, it works great in both!" />
-        <meta property="og:image" content="https://guides.gamepressure.com/darksouls/gfx/word/464924671.jpg" />
+        <meta
+          property="og:description"
+          content="A helper app for playing Dark Souls: The Board Game. Use it on your phone or desktop browser, it works great in both!"
+        />
+        <meta
+          property="og:image"
+          content="https://guides.gamepressure.com/darksouls/gfx/word/464924671.jpg"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_US" />
       </Helmet>
@@ -20,8 +26,7 @@ function App() {
         <h1>Dark Souls</h1>
         <h2>The Board Game</h2>
       </header>
-      <main>
-      </main>
+      <main />
     </div>
   )
 }
