0c8e8e94ab50c92118f694afc9953d91415feacc
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-03-19 17:30:32 -0300

    feat(home): tweak design and add gap between action group section

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,25 +3,23 @@ import './Home.css'
 export default function Home() {
   return (
     <div id="base-container">
-      <div id="main-container">
-        <main className="container">
-          <section id="main-header" className="container">
-            <h1 id="main-title">Solaire</h1>
-            <p id="main-subtitle">
-              An App for Playing Dark Souls: The Board Game
-            </p>
-          </section>
-          <section className="action-group">
-            <p>Push Action</p>
-            <button type="button" className="btn btn-encounter">
-              Regular Encounter
-            </button>
-            <button type="button" className="btn btn-encounter">
-              Boss or Mini-Boss Encounter
-            </button>
-          </section>
-        </main>
-      </div>
+      <main id="main-container" className="container">
+        <section id="main-header" className="container">
+          <h1 id="main-title">Solaire</h1>
+          <p id="main-subtitle">
+            An App for Playing Dark Souls: The Board Game
+          </p>
+        </section>
+        <section className="action-group">
+          <p>Push Action</p>
+          <button type="button" className="btn btn-encounter">
+            Regular Encounter
+          </button>
+          <button type="button" className="btn btn-encounter">
+            Boss or Mini-Boss Encounter
+          </button>
+        </section>
+      </main>
       <footer>
         <p>website created with no commercial interest</p>
       </footer>
