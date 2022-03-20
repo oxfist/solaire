1939057ae91ae0881b40962ba32878269b305fc8
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-03-20 12:11:03 -0300

    feat(home): add placeholder background images to encounter buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,15 +7,23 @@ export default function Home() {
         <section id="main-header" className="container">
           <h1 id="main-title">Solaire</h1>
           <p id="main-subtitle">
-            An App for Playing Dark Souls: The Board Game
+            An App for Playing <em>Dark Souls: The Board Game</em>
           </p>
         </section>
         <section className="action-group">
           <p>Push Action</p>
-          <button type="button" className="btn btn-encounter">
+          <button
+            type="button"
+            id="regular-encounter"
+            className="btn btn-encounter"
+          >
             Regular Encounter
           </button>
-          <button type="button" className="btn btn-encounter">
+          <button
+            type="button"
+            id="boss-encounter"
+            className="btn btn-encounter"
+          >
             Boss or Mini-Boss Encounter
           </button>
         </section>
